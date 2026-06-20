1967c50ff45d5efad171800e285f013626e5d257
Jayant Hegde Kageri <jayantkageri@gmail.com>
2026-06-20 23:53:23 +0530

    add headers (#44)

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ const contentSecurityPolicy = [
   "font-src 'self' data: https://fonts.gstatic.com",
   "img-src 'self' data: blob: https://res.cloudinary.com https://firebasestorage.googleapis.com https://*.googleusercontent.com https://www.gstatic.com",
   "connect-src 'self' https://*.googleapis.com https://*.firebaseio.com wss://*.firebaseio.com https://*.firebaseapp.com https://apis.google.com https://accounts.google.com https://cdn.jsdelivr.net https://www.google.com",
-  "frame-src 'self' https://www.google.com https://*.firebaseapp.com https://accounts.google.com https://apis.google.com",
+  "frame-src 'self' https://www.google.com https://*.firebaseapp.com https://accounts.google.com https://apis.google.com https://www.youtube.com https://maps.google.com",
   "worker-src 'self' blob:",
 ].join("; ");
 
