b205f6f1bdb32b974007c1f240fc6aece92a023c
Jayant Hegde Kageri <jayantkageri@gmail.com>
2026-06-21 00:40:45 +0530

    Add CSP of Image Icon (#46)
    * add headers
    
    * changes
    
    * Add Flag Source to CSP

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const contentSecurityPolicy = [
   "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.google.com https://www.gstatic.com https://apis.google.com",
   "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
   "font-src 'self' data: https://fonts.gstatic.com",
-  "img-src 'self' data: blob: https://res.cloudinary.com https://firebasestorage.googleapis.com https://*.googleusercontent.com https://www.gstatic.com",
+  "img-src 'self' data: blob: https://res.cloudinary.com https://firebasestorage.googleapis.com https://*.googleusercontent.com https://www.gstatic.com https://purecatamphetamine.github.io",
   "connect-src 'self' https://*.googleapis.com https://*.firebaseio.com wss://*.firebaseio.com https://*.firebaseapp.com https://apis.google.com https://accounts.google.com https://cdn.jsdelivr.net https://www.google.com",
   "frame-src 'self' https://www.google.com https://*.firebaseapp.com https://accounts.google.com https://apis.google.com https://www.youtube.com https://maps.google.com",
   "worker-src 'self' blob:",
